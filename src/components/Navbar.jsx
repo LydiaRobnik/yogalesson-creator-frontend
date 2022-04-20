a7ec5508594667b9904c6a2fb0ed17f617619027
Thomas <schmiddla@gmail.com>
2022-04-20 11:58:01 +0200

    style stuff

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,5 +2,20 @@ import React from 'react';
 import './style/navbar.scss';
 
 export default function Navbar() {
-  return <nav>Navbar</nav>;
+  return (
+    <nav className="flex gap-5 pt-4">
+      <a
+        href="https://github.com/TomEsDE/yogalesson-creator-frontend"
+        target={'_blank'}
+      >
+        Github Frontend
+      </a>
+      <a
+        href="https://github.com/TomEsDE/yogalesson-creator-backend"
+        target={'_blank'}
+      >
+        Github Backend
+      </a>
+    </nav>
+  );
 }
