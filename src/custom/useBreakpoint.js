7de73d14a50644aafa41984f8e19aad7389fde43
Thomas <schmiddla@gmail.com>
2022-04-26 10:22:56 +0200

    tailwind breakpoints

--- src/custom/useBreakpoint.js
+++ src/custom/useBreakpoint.js
@@ -2,14 +2,18 @@ import { useState, useEffect } from 'react';
 import throttle from 'lodash.throttle';
 
 const getDeviceConfig = (width) => {
-  if (width < 320) {
+  if (width < 640) {
     return 'xs';
-  } else if (width >= 320 && width < 720) {
+  } else if (width >= 640 && width < 768) {
     return 'sm';
-  } else if (width >= 720 && width < 1024) {
+  } else if (width >= 768 && width < 1024) {
     return 'md';
-  } else if (width >= 1024) {
+  } else if (width >= 1024 && width < 1280) {
     return 'lg';
+  } else if (width >= 1280 && width < 1536) {
+    return 'xl';
+  } else if (width >= 1536) {
+    return '2xl';
   }
 };
 
