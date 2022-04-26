59c6c70d3481f7f41f44c7c0a1489021dc54e28f
Lydia Robnik <84912419+LydiaRobnik@users.noreply.github.com>
2022-04-26 15:48:37 +0200

    dashboard structure

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,46 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import http from '../../api/http-common';
-import './dashboard.scss';
+import React, { useEffect, useState } from "react";
+import http from "../../api/http-common";
+import "./dashboard.scss";
 
 export default function Dashboard() {
-  const [asanas, setAsanas] = useState([]);
+	const [asanas, setAsanas] = useState([]);
+	const [classes, setClasses] = useState([]);
 
-  useEffect(() => {
-    console.log('useEffect');
-    http()
-      .get('/asana')
-      .then((resp) => {
-        console.log('asanas', resp.data);
-        setAsanas(resp.data);
-      });
+	// useEffect(() => {
+	//   console.log('useEffect');
+	//   http()
+	//     .get('/asanas')
+	//     .then((resp) => {
+	//       console.log('asanas', resp.data);
+	//       setAsanas(resp.data);
+	//     });
 
-    return () => {};
-  }, []);
+	//   return () => {};
+	// }, []);
 
-  return (
-    <div className="home flex flex-col items-center color-primary">
-      <div className="font-bold text-3xl md:text-7xl hover:text-blue-300 pb-5">
-        Yoga Lesson Creator with {asanas.length} Asanas
-      </div>
-      <div className="flex flex-wrap gap-4">
-        {asanas.map((asana) => (
-          <div
-            key={asana.id}
-            className="w-20 md:w-40 flex flex-col items-center font-raleway border-2 bg-secondary color-1 border-gray-600 rounded-lg p-4"
-          >
-            <img className="invert" src={asana.img_url} alt="" />
-            <div className="flex-1 text-xs text-center md:text-sm font-bold pt-4">
-              {asana.asana?.sanskrit}
-            </div>
-            <div className="flex-1 text-xs text-center md:text-sm italic pt-1">
-              {asana.asana?.en}
-            </div>
-            <div className="flex-1 text-md text-center md:text-xl text-amber-400 font-peg pt-1">
-              {asana.level?.en}
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<>
+			<main>
+				<div>
+					<button className="btn-blue btn-blue:hover">new class</button>
+					<button className="btn-blue btn-blue:hover">random class</button>
+				</div>
+				<h2>recently used</h2>
+				{}
+				<h2>favorites</h2>
+				{}
+			</main>
+		</>
+	);
 }
---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Section = () => {
+	return <div>Section</div>;
+};
+
+export default Section;
