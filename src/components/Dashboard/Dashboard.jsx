2c20feb622aa6260bbadbfdf21bd3014759a8063
Lydia Robnik <84912419+LydiaRobnik@users.noreply.github.com>
2022-04-26 17:43:38 +0200

    background color changed to white in app.scss

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -3,33 +3,33 @@ import http from "../../api/http-common";
 import "./dashboard.scss";
 
 export default function Dashboard() {
-	const [asanas, setAsanas] = useState([]);
-	const [classes, setClasses] = useState([]);
+  const [asanas, setAsanas] = useState([]);
+  const [classes, setClasses] = useState([]);
 
-	// useEffect(() => {
-	//   console.log('useEffect');
-	//   http()
-	//     .get('/asanas')
-	//     .then((resp) => {
-	//       console.log('asanas', resp.data);
-	//       setAsanas(resp.data);
-	//     });
+  // useEffect(() => {
+  //   console.log('useEffect');
+  //   http(true)
+  //     .get('/class/latest')
+  //     .then((resp) => {
+  //       console.log('asanas', resp.data);
+  //       setClasses(resp.data);
+  //     });
 
-	//   return () => {};
-	// }, []);
+  //   return () => {};
+  // }, []);
 
-	return (
-		<>
-			<main>
-				<div>
-					<button className="btn-blue btn-blue:hover">new class</button>
-					<button className="btn-red btn-blue:hover">random class</button>
-				</div>
-				<h2>recently used</h2>
-				{}
-				<h2>favorites</h2>
-				{}
-			</main>
-		</>
-	);
+  return (
+    <>
+      <main>
+        <div>
+          <button className="btn-blue btn-blue:hover">new class</button>
+          <button className="btn-red btn-blue:hover">random class</button>
+        </div>
+        <h2>recently used</h2>
+        {}
+        <h2>favorites</h2>
+        {}
+      </main>
+    </>
+  );
 }
