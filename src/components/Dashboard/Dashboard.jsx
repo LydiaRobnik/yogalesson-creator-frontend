9e5937477e7da86b12e3fd2bfca4cc5425d99951
Lydia Robnik <84912419+LydiaRobnik@users.noreply.github.com>
2022-04-27 18:02:14 +0200

    dashboard is working with mock data

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,35 +1,93 @@
 import React, { useEffect, useState } from "react";
 import http from "../../api/http-common";
 import "./dashboard.scss";
+import useBreakpoint from "../../custom/useBreakpoint";
+
+// import components
+import ClassCard from "../ClassCard/ClassCard.jsx";
+
+// import data
+import data from "../../assets/class";
 
 export default function Dashboard() {
-  const [asanas, setAsanas] = useState([]);
-  const [classes, setClasses] = useState([]);
+  // states
+  const [classes, setClasses] = useState(data);
+  const point = useBreakpoint();
+  // const latestClasses = classes.slice();
 
+  // sort classes by date
+  classes.sort((a, b) => {
+    return new Date(b.modifiedAt) - new Date(a.modifiedAt);
+  });
+
+  // fetches
   // useEffect(() => {
   //   console.log('useEffect');
   //   http(true)
-  //     .get('/class/latest')
+  //     .get('/class')
   //     .then((resp) => {
-  //       console.log('asanas', resp.data);
+  //       console.log('latest classes', resp.data);
   //       setClasses(resp.data);
   //     });
-
-  //   return () => {};
   // }, []);
+
+  const gridResponsibility = () => {
+    if (point === "sm") {
+      return "grid-cols-2";
+    } else if (point === "md") {
+      return "grid-cols-3";
+    } else if (point === "lg") {
+      return "grid-cols-4";
+    } else if (point === "xl") {
+      return "grid-cols-5";
+    } else if (point === "2xl") {
+      return "grid-cols-6";
+    } else {
+      return "grid-cols-1";
+    }
+  };
 
   return (
     <>
-      <main>
-        <div>
-          <button className="btn-blue btn-blue:hover">new class</button>
-          <button className="btn-red btn-blue:hover">random class</button>
+      <div
+        className={`p-4 w-full flex flex-row ${
+          point === "xs" ? "justify-center" : "justify-start"
+        }`}
+      >
+        <button className="btn-blue btn-blue:hover mx-2">new class</button>
+        <button className="btn-red btn-blue:hover mx-2">random class</button>
+      </div>
+
+      <div
+        className="px-6
+       w-full"
+      >
+        <h2 className="color-blue-darkest">recently used</h2>
+        <div className={`justify-center grid gap-4 ${gridResponsibility()}`}>
+          {classes &&
+            classes.map((classItem) => (
+              <>
+                <ClassCard classItem={classItem} />
+              </>
+            ))}
         </div>
-        <h2>recently used</h2>
-        {}
-        <h2>favorites</h2>
-        {}
-      </main>
+      </div>
+
+      <div>
+        <h2 className="color-blue-darkest">favorites</h2>
+        <div
+          className={`justify-center grid gap-4 ${
+            point === "sm" ? "grid-cols-3" : "grid-cols-2"
+          }`}
+        >
+          {classes &&
+            classes.map((classItem) => (
+              <>
+                <ClassCard classItem={classItem} />
+              </>
+            ))}
+        </div>
+      </div>
     </>
   );
 }
