3074c324ccc53eda871a0caa62db43892cc1fbfc
Lydia Robnik <84912419+LydiaRobnik@users.noreply.github.com>
2022-04-28 15:53:20 +0200

    loading animation added to dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -9,9 +9,10 @@ import ClassCard from "../ClassCard/ClassCard.jsx";
 // import data
 import data from "../../assets/class";
 
-export default function Dashboard() {
+export default function Dashboard({ loading, setLoading }) {
   // states
   const [classes, setClasses] = useState(data);
+  const [asanas, setAsanas] = useState(data);
   const point = useBreakpoint();
   // const latestClasses = classes.slice();
 
@@ -22,13 +23,20 @@ export default function Dashboard() {
 
   // fetches
   // useEffect(() => {
-  //   console.log('useEffect');
-  //   http(true)
-  //     .get('/class')
-  //     .then((resp) => {
-  //       console.log('latest classes', resp.data);
-  //       setClasses(resp.data);
-  //     });
+  //   console.log("useEffect");
+
+  //   const fetchData = () => {
+  //     setLoading(true);
+  //     http(true)
+  //       .get("/class")
+  //       .then((resp) => {
+  //         console.log("latest classes", resp.data);
+  //         setAsanas(resp.data);
+  //       });
+  //     setLoading(false);
+  //   };
+
+  //   fetchData();
   // }, []);
 
   const gridResponsibility = () => {
@@ -49,100 +57,117 @@ export default function Dashboard() {
 
   return (
     <>
-      <div
-        className={`p-4 w-full flex flex-row ${
-          point === "xs" ? "justify-center" : "justify-start"
-        }`}
-      >
-        <button className="btn-blue btn-blue:hover mx-2 flex flex-row items-center">
-          <p className="font-material inline pr-2">add</p>
-          <p className="inline pt-1">new class</p>
-        </button>
-        <button className="btn-red btn-blue:hover mx-2 flex flex-row items-center">
-          <p className="font-material inline pr-2">add</p>
-          <p className="inline pt-1">random class</p>
-        </button>
-      </div>
+      {loading && (
+        <lottie-player
+          src="https://assets1.lottiefiles.com/packages/lf20_s00z9gco.json"
+          background="transparent"
+          speed="1"
+          style={{ width: "300px", height: "300px" }}
+          loop
+          autoplay
+        ></lottie-player>
+      )}
 
-      <div
-        className={`px-6
-       w-full ${point === "xs" ? "justify-center" : "justify-start"}`}
-      >
-        {classes.length === 0 && (
-          <>
-            <div className="flex flex-col justify-center">
-              <span className="material-symbols-outlined color-blue-darkest text-center text-4xl p-2">
-                note_add
-              </span>
-              <h3 className="color-blue-darkest text-center font-bold">
-                No classes
-              </h3>
-              <p className="color-blue-darkest text-center">
-                Get started by creating a new class.
-              </p>
+      {!loading && (
+        <>
+          <div
+            className={`p-4 w-full flex flex-row ${
+              point === "xs" ? "justify-center" : "justify-start"
+            }`}
+          >
+            <button className="btn-blue btn-blue:hover mx-2 flex flex-row items-center">
+              <p className="font-material inline pr-2">add</p>
+              <p className="inline pt-1">new class</p>
+            </button>
+            <button className="btn-red btn-blue:hover mx-2 flex flex-row items-center">
+              <p className="font-material inline pr-2">add</p>
+              <p className="inline pt-1">random class</p>
+            </button>
+          </div>
+
+          <div
+            className={`px-6
+        w-full ${point === "xs" ? "justify-center" : "justify-start"}`}
+          >
+            {classes.length === 0 && (
+              <>
+                <div className="flex flex-col justify-center">
+                  <span className="material-symbols-outlined color-blue-darkest text-center text-4xl p-2">
+                    note_add
+                  </span>
+                  <h3 className="color-blue-darkest text-center font-bold">
+                    No classes
+                  </h3>
+                  <p className="color-blue-darkest text-center">
+                    Get started by creating a new class.
+                  </p>
+                </div>
+              </>
+            )}
+            {classes.length > 0 && (
+              <h2
+                className={`color-blue-darkest text-4xl mt-8 mb-4 ${
+                  point === "xs" ? "text-center" : "text-start"
+                }`}
+              >
+                recently used
+              </h2>
+            )}
+            <div
+              className={`justify-center grid gap-4 ${gridResponsibility()}`}
+            >
+              {classes &&
+                classes.map((classItem) => (
+                  <>
+                    <ClassCard classItem={classItem} />
+                  </>
+                ))}
             </div>
-          </>
-        )}
-        {classes.length > 0 && (
-          <h2
-            className={`color-blue-darkest text-4xl mt-8 mb-4 ${
-              point === "xs" ? "text-center" : "text-start"
-            }`}
-          >
-            recently used
-          </h2>
-        )}
-        <div className={`justify-center grid gap-4 ${gridResponsibility()}`}>
-          {classes &&
-            classes.map((classItem) => (
-              <>
-                <ClassCard classItem={classItem} />
-              </>
-            ))}
-        </div>
-      </div>
+          </div>
 
-      <div
-        className={`px-6
+          <div
+            className={`px-6
        w-full ${point === "xs" ? "justify-center" : "justify-start"}`}
-      >
-        {/* edit the following line, after addition of the favorits key into backend */}
-        {classes.length > 0 ||
-          (classes.length.favorit === 0 && (
-            <>
-              <div className="flex flex-col justify-center">
-                <span class="material-symbols-outlined color-blue-darkest text-center text-4xl p-2">
-                  folder_special
-                </span>
-                <h3 className="color-blue-darkest text-center font-bold">
-                  No favorites defined
-                </h3>
-                <p className="color-blue-darkest text-center">
-                  Get started by marking them.
-                </p>
-              </div>
-            </>
-          ))}
-        {classes.length > 0 && (
-          <h2
-            className={`color-blue-darkest text-4xl mt-8 mb-4 ${
-              point === "xs" ? "text-center" : "text-start"
-            }`}
           >
-            favorites
-          </h2>
-        )}
-        <div
-          className={`justify-center grid gap-4  mb-8 ${gridResponsibility()}`}
-        >
-          {classes &&
-            classes.map((classItem) => (
-              <>
-                <ClassCard classItem={classItem} />
-              </>
-            ))}
-        </div>
-      </div>
+            {/* edit the following line, after addition of the favorits key into backend */}
+            {classes.length > 0 ||
+              (classes.length.favorit === 0 && (
+                <>
+                  <div className="flex flex-col justify-center">
+                    <span class="material-symbols-outlined color-blue-darkest text-center text-4xl p-2">
+                      folder_special
+                    </span>
+                    <h3 className="color-blue-darkest text-center font-bold">
+                      No favorites defined
+                    </h3>
+                    <p className="color-blue-darkest text-center">
+                      Get started by marking them.
+                    </p>
+                  </div>
+                </>
+              ))}
+            {classes.length > 0 && (
+              <h2
+                className={`color-blue-darkest text-4xl mt-8 mb-4 ${
+                  point === "xs" ? "text-center" : "text-start"
+                }`}
+              >
+                favorites
+              </h2>
+            )}
+            <div
+              className={`justify-center grid gap-4  mb-8 ${gridResponsibility()}`}
+            >
+              {classes &&
+                classes.map((classItem) => (
+                  <>
+                    <ClassCard classItem={classItem} />
+                  </>
+                ))}
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 }
