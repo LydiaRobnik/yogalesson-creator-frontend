b5d0cc6981acd7fad9c55fa5f2765ee6aca03f87
VLacroixB <97441265+VLacroixB@users.noreply.github.com>
2022-04-28 15:02:52 +0200

    z-index on home for responsiveness

--- src/components/Sections/Section1.jsx
+++ src/components/Sections/Section1.jsx
@@ -6,30 +6,34 @@ import flowers from '../../pictures/flowers.jpg';
 export default function Section1() {
   return (
     <div className="section-container">
-      <div className="sectionTitleColor w-1/2 flex-col items-center justify-center text-6xl font-bold hidden md:flex border-2 border-red-500">
-        <h1 className="sectionTitle font-light">Beginners Class</h1>
-        <div className="grid gap-4 grid-cols-4 grid-rows-4">
-          <div className="bg-clip-border p-16 border-4 bg-white rounded-lg"></div>
-          <div className="bg-clip-border p-16 border-4 bg-white rounded-lg"></div>
-          <div className="bg-clip-border p-16 border-4 bg-white rounded-lg"></div>
-          <div className="bg-clip-border p-16 border-4 bg-white rounded-lg"></div>
-          <div className="bg-clip-border p-16 border-4 bg-white rounded-lg"></div>
-          <div className="bg-clip-border p-16 border-4 bg-white rounded-lg"></div>
-          <div className="bg-clip-border p-16 border-4 bg-white rounded-lg"></div>
-          <div className="bg-clip-border p-16 border-4 bg-white rounded-lg"></div>
+<div className="md:flex">
+      <div className="sectionTitleColor md:w-1/2 flex-col items-center justify-center text-6xl font-bold md:shrink-0 border-2 border-red-500">
+        <div className="sectionTitle flex items-center justify-center font-light text-10xl leading-relaxed">Beginners Class</div>
+         <div className="max-w-screen-lg mx-auto p-4">
+          <div className="grid gap-4 grid-cols-4">
+           <div className="bg-clip-border m-4 p-16 border-4 bg-white rounded-lg"></div>
+           <div className="bg-clip-border m-4 p-16 border-4 bg-white rounded-lg"></div>
+           <div className="bg-clip-border m-4 p-16 border-4 bg-white rounded-lg"></div>
+           <div className="bg-clip-border m-4 p-16 border-4 bg-white rounded-lg"></div>
+           <div className="bg-clip-border m-4 p-16 border-4 bg-white rounded-lg"></div>
+           <div className="bg-clip-border m-4 p-16 border-4 bg-white rounded-lg"></div>
+           <div className="bg-clip-border m-4 p-16 border-4 bg-white rounded-lg"></div>
+           <div className="bg-clip-border m-4 p-16 border-4 bg-white rounded-lg"></div>
         </div>
+      </div>
     </div>
-    <div className="w-full border-2 border-blue-500 md:w-1/2 flex items-center justify-center relative">
+    <div className="w-full border-2 border-blue-500 flex items-center justify-center relative md:flex md:w-1/2">
       <img className="object-cover w-full h-full bg-bottom" src={flowers} alt="flowers on white background"/>
-      <p className="featureText p-4 m-12 absolute bottom-50 text-5xl text-left bg-red-500 bg-opacity-20 font-light">
+      <div className="featureText flex items-center justify-center leading-relaxed p-5 m-12 absolute bottom-50 text-4xl text-left bg-red-500 bg-opacity-20 backdrop-blur-md font-light">
 Use our templates to structure your class. <br></br>
 Select exercises and sort them. Create reusable sequenzes 
-and save your classes.</p>
+and save your classes.</div>
     </div>
-    <div className="md:hidden p-8 absolute text-4xl bottom-5 bg-red-500 bg-opacity-50">
+    {/* <div className="md:hidden p-8 absolute text-4xl bottom-5 bg-red-500 bg-opacity-50">
         <div>some text</div>
         <div>some text</div>
-    </div>
+    </div> */}
+</div>
  </div>
   );
 }
--- src/components/Sections/Section2.jsx
+++ src/components/Sections/Section2.jsx
@@ -5,18 +5,19 @@ import structure from '../../pictures/structure.avif';
 export default function Section1() {
   return (
     <div className="section-container">
-    <div className="w-1/2 flex-col items-center justify-center text-6xl font-bold hidden md:flex border-2 border-red-500">
-        <p className="featureText p-4 m-12 absolute bottom-50 text-5xl text-left font-light">Stucture your course fast and with ease.<br></br>
-      Keep an eye onto the golden thread.<br></br>
-      Prepare texts for Shavasana.</p>
-    </div>
-    <div className="md:hidden p-8 absolute text-4xl bottom-5 bg-red-500 bg-opacity-50">
-          <div>text overlay</div>
-          <div>text overlay</div>
-          <div>text overlay</div>
+    <div className="w-1/2 flex-col items-center justify-center hidden md:flex border-2 border-red-500">
+        <div className="featureText leading-relaxed p-5 m-12 absolute bottom-50 text-4xl text-left font-light">
+        Stucture your course fast and with ease.<br></br>
+        Keep an eye onto the golden thread.<br></br>
+        Prepare texts for Shavasana.</div>
     </div>
     <div className="w-full border-2 border-blue-500 md:w-1/2 flex items-center justify-center relative">
-      <img className="object-cover" src={structure} alt="girl writing down to the planner"/>
+     <img className="object-cover w-full h-full bg-bottom" src={structure} alt="girl writing down to the planner"/>
+    </div>
+    <div className="featureText md:hidden p-5 m-12 absolute flex items-center justify-center leading-relaxed bottom-50 text-4xl text-left bg-red-500 bg-opacity-20 backdrop-blur-md font-light z-1">
+      Stucture your course fast and with ease.<br></br>
+      Keep an eye onto the golden thread.<br></br>
+      Prepare texts for Shavasana. 
     </div>
  </div>
   );
