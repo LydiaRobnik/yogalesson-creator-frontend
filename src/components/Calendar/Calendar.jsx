127f03dd6e50b33941a6cbf7f580eb5ff8b2d718
Thomas <schmiddla@gmail.com>
2022-05-02 17:50:41 +0200

    calendar tests

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,5 +1,8 @@
 import React, { useContext, useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import FullCalendar from '@fullcalendar/react'; // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
+
 import asanaService from '../../api/asanaService';
 import { AuthContext } from '../../context/AuthContext';
 import './calendar.scss';
@@ -10,14 +13,21 @@ export default function Calendar() {
   const [error, setError] = useState('');
 
   const navigate = useNavigate();
+  function handleDateClick(arg) {
+    console.log('handleDateClick', arg);
+  }
 
   useEffect(() => {
     return () => {};
   }, [loggedIn]);
 
   return (
-    <div id="div-calendar" className="text-black">
-      Calendar
+    <div id="div-calendar" className="text-black w-full h-full">
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth"
+        // dateClick={handleDateClick}
+      />
     </div>
   );
 }
