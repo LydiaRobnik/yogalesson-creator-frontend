769e56213031115fb54cf73c027c96158a20dc40
Michael-Andreas <michaelandreas246@gmail.com>
2022-05-03 15:05:22 +0200

    login and navbar fixed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,11 +4,12 @@ import ReactFullpage from "@fullpage/react-fullpage";
 import "fullpage.js/vendors/scrolloverflow"; // Optional. When using scrollOverflow:true
 import Navbar from "../Navbar/Navbar";
 import "./home.scss";
-import Section1 from '../Sections/Section1';
-import Section2 from '../Sections/Section2';
-import Section3 from '../Sections/Section3';
+import Section1 from "../Sections/Section1";
+import Section2 from "../Sections/Section2";
+import Section3 from "../Sections/Section3";
 import TestResponsive from "./TestResponsive";
 import Footer from "../Footer/Footer.jsx";
+import Hero from "./Hero.jsx";
 
 export default function Home() {
   const navigate = useNavigate();
@@ -21,48 +22,31 @@ export default function Home() {
         render={({ state, fullpageApi }) => {
           return (
             <ReactFullpage.Wrapper>
+              <div className="top section color-primary h-full w-full">
+                <div className="h-full">
+                  <Navbar className="z-1" />
+                  <Hero />
+                </div>
+              </div>
+
+              <div className="section color-primary">
+                <Section1 />
+              </div>
+              <div className="section color-primary">
+                <Section2 />
+              </div>
+              <div className="section color-primary">
+                <Section3 />
+              </div>
               <div className="section color-primary">
                 <div className="h-full">
                   <TestResponsive />
                 </div>
               </div>
 
-              <div className="top section color-primary h-full w-full">
-                <div className="h-full">
-                  <Navbar className="z-1"/>
-                  <div className="bluewindow">
-                  <div className="hero ">
-                    <h2 className="color-beige-light font-moontime text-7xl mt-5 mb-5">yoga class planning</h2>
-                    <h3 className="font-bold">easy and quick</h3>
-                    <p>create well structured yoga classes</p>
-                  </div>
-            </div>
-              <div className="divdivdiv">
-              <button
-              type="submit"
-              className="bg-red-700 hover:bg-red-500 text-white font-bold py-2 px-10"
-              >
-              try it now
-            </button>
-              </div>
-              </div>
-
-              </div>
-
-              <div className="section color-primary">
-                    <Section1 />
-                  </div>
-                  <div className="section color-primary">
-                    <Section2 />
-                  </div>
-                  <div className="section color-primary">
-                    <Section3 />
-                  </div>
-
               <div className="section color-primary text-5xl">
                 <Footer />
               </div>
-            
             </ReactFullpage.Wrapper>
           );
         }}
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,235 +1,246 @@
-import React, { useContext, useState } from 'react';
-import { NavLink, Link } from 'react-router-dom';
-import { AuthContext } from '../../context/AuthContext';
-import './navbar.scss';
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+import React, { useContext, useState } from "react";
+import { NavLink, Link } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+import "./navbar.scss";
+import { Fragment } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
 import LoginModal from "../Login/LoginModal.jsx";
+import SignupModal from "../Login/SignupModal";
 
 const navigation = [
-  { name: 'Dashboard', path: '/user/dashboard', current: false },
-  { name: 'Classes', path: '/user/planner', current: false },
-  { name: 'Calendar', path: '/', current: false },
-]
+  { name: "Dashboard", path: "/user/dashboard", current: false },
+  { name: "Classes", path: "/user/planner", current: false },
+  { name: "Calendar", path: "/", current: false }
+];
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 export default function Navbar() {
   const { loggedIn, login, logout, user } = useContext(AuthContext);
-  const [name, setName] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-  const [open, setOpen] = useState(true)
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [open, setOpen] = useState(true);
+  const [ModalOpen, setModalOpen] = useState(false);
+  const [SignupModalOpen, setSignupModalOpen] = useState(false);
 
   async function handleLogin(e) {
     e.preventDefault();
-    setError('');
+    setError("");
 
     const result = await login({
       user: name,
-      type: 'username',
-      password: password,
+      type: "username",
+      password: password
     });
 
     if (!result) {
-      setError('Invalid username or password!');
+      setError("Invalid username or password!");
     } else {
-      setName('');
-      setPassword('');
+      setName("");
+      setPassword("");
     }
   }
 
   return (
     <>
-    <Disclosure as="nav" className="bg-light color">
-      {({ open }) => (
-        <>
-          <div className="navbar max-w-7xl mx-auto px-2 sm:px-6">
-            <div className="relative flex items-center justify-between h-16 mx-auto">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden mx-auto">
-                {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
-              </div>
-              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex-shrink-0 flex items-center">
-                  <img
-                    className="block lg:hidden h-8 w-auto height"
-                    src="https://yogalesson-createor-backend.herokuapp.com/images/Logo_250x250px.png"
-                    alt="Workflow"
-                  />
-                  <img
-                    className="hidden lg:block h-8 w-auto"
-                    src="https://yogalesson-createor-backend.herokuapp.com/images/Logo_250x250px.png"
-                    alt="Workflow"
-                  />
-                  <p className="color-blue-darkest">PlanAsana<br></br>
-                  YOGA OFF THE MAT</p>
-                </div> 
-                    
+      <LoginModal ModalOpen={ModalOpen} setModalOpen={setModalOpen} />
+      <SignupModal
+        SignupModalOpen={SignupModalOpen}
+        setSignupModalOpen={setSignupModalOpen}
+      />
+      <Disclosure as="nav" className="bg-light color">
+        {({ open }) => (
+          <>
+            <div className="navbar max-w-7xl mx-auto px-2 sm:px-6">
+              <div className="relative flex items-center justify-between h-16 mx-auto">
                 {loggedIn && (
-                <div className="hidden sm:block sm:ml-6 ">
-                  <div className="flex space-x-4">
-                    {navigation.map((item) => (
-                      <NavLink
-                        key={item.name}
-                        href={item.href}
-                        to={item.path}
-                        className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'color-blue-darkest hover:bg-gray-700 hover:text-white',
-                          'px-3 py-2 rounded-md text-sm font-medium'
-                        )}
-                        aria-current={item.current ? 'page' : undefined}
-                      >
-                        {item.name}
-                      </NavLink>
-                    ))}
+                  <div className="absolute inset-y-0 left-0 flex items-center sm:hidden mx-auto">
+                    {/* Mobile menu button*/}
+                    <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                      <span className="sr-only">Open main menu</span>
+                      {open ? (
+                        <XIcon className="block h-6 w-6" aria-hidden="true" />
+                      ) : (
+                        <MenuIcon
+                          className="block h-6 w-6"
+                          aria-hidden="true"
+                        />
+                      )}
+                    </Disclosure.Button>
                   </div>
+                )}
+                <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                  <div className="flex-shrink-0 flex items-center">
+                    <img
+                      className="block lg:hidden h-8 w-auto height"
+                      src="https://yogalesson-createor-backend.herokuapp.com/images/Logo_250x250px.png"
+                      alt="Workflow"
+                    />
+                    <img
+                      className="hidden lg:block h-8 w-auto"
+                      src="https://yogalesson-createor-backend.herokuapp.com/images/Logo_250x250px.png"
+                      alt="Workflow"
+                    />
+                    <p className="color-blue-darkest">
+                      PlanAsana<br></br>
+                      YOGA OFF THE MAT
+                    </p>
+                  </div>
+
+                  {loggedIn && (
+                    <div className="hidden sm:block sm:ml-6 ">
+                      <div className="flex space-x-4">
+                        {navigation.map((item) => (
+                          <NavLink
+                            key={item.name}
+                            href={item.href}
+                            to={item.path}
+                            className={classNames(
+                              item.current
+                                ? "bg-gray-900 text-white"
+                                : "color-blue-darkest hover:bg-gray-700 hover:text-white",
+                              "px-3 py-2 rounded-md text-sm font-medium"
+                            )}
+                            aria-current={item.current ? "page" : undefined}
+                          >
+                            {item.name}
+                          </NavLink>
+                        ))}
+                      </div>
+                    </div>
+                  )}
                 </div>
-                )}
-              </div>
 
-              <div className="color-blue-darkest">
-        {loggedIn ? (
-          <div>
-            <span className="pr-2 text-sm color-5">Hi {user.name}!</span>
-          </div>
-        ) : (
-          <form onSubmit={handleLogin} className="flex gap-4 color-primary">
-            <input
-              className="px-1 rounded-lg w-32"
-              type="text"
-              minLength={3}
-              onChange={(e) => setName(e.target.value)}
-              placeholder="Username"
-            />
-            <input
-              className="px-1 rounded-lg w-32"
-              type="password"
-              minLength={4}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="Password"
-            />
-            <button
-              type="submit"
-              className="bg-red-700 hover:bg-red-500 text-white font-bold py-2 px-10"
-            >
-              login
-            </button>
-            <button
-              type=""
-              className="hover:underline text-color-blue-darkest py-2 px-10"
-            >
-              sign up
-            </button>
-            {error && (
-              <div className="text-sm pl-5 pt-2 text-red-500">{error}</div>
-            )}
-          </form>
-        )}
-      </div>
-
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-              <button
-              className="bg-red-700 hover:bg-red-500 text-white font-bold "
-              onClick={() => setOpen(true)}
-            >
-              login
-            </button>
-            <p className="text-black">{open ? "true" : "false"}</p>
-                {/* Profile dropdown */}
-                {loggedIn && (
-                <Menu as="div" className="ml-3 relative">
-                  <div>
-                    <Menu.Button className="">
-                      <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Your Profile
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Settings
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                          href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                            onClick={() => logout()}
-                          >
-                            Sign out
-                          </a>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
-                )}
-              </div>
-            </div>
-          </div>
-
-          <Disclosure.Panel className="sm:hidden">
-            <div className="px-2 pt-2 pb-3 space-y-1">
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                    'block px-3 py-2 rounded-md text-base font-medium'
+                <div className="color-blue-darkest">
+                  {loggedIn ? (
+                    <div>
+                      <span className="pr-2 text-sm color-5">
+                        Hi {user.name}!
+                      </span>
+                    </div>
+                  ) : (
+                    <>
+                      <button
+                        className="bg-red hover:bg-red-500 text-white font-bold py-3 px-10"
+                        onClick={() => setModalOpen(true)}
+                      >
+                        login
+                      </button>
+                      <button
+                        className="hover:underline text-color-blue-darkest py-2 px-10"
+                        onClick={() => setSignupModalOpen(true)}
+                      >
+                        sign up
+                      </button>
+                      {error && (
+                        <div className="text-sm pl-5 pt-2 text-red-500">
+                          {error}
+                        </div>
+                      )}
+                    </>
                   )}
-                  aria-current={item.current ? 'page' : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
+                </div>
+
+                <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                  {/* Profile dropdown */}
+                  {loggedIn && (
+                    <Menu as="div" className="ml-3 relative">
+                      <div>
+                        <Menu.Button className="">
+                          <span className="sr-only">Open user menu</span>
+                          <img
+                            className="h-8 w-8 rounded-full"
+                            src="https://www.kindpng.com/picc/m/21-211456_user-icon-hd-png-download.png"
+                            alt=""
+                          />
+                        </Menu.Button>
+                      </div>
+                      <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                      >
+                        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                          <Menu.Item>
+                            {({ active }) => (
+                              <a
+                                href="#"
+                                className={classNames(
+                                  active ? "bg-gray-100" : "",
+                                  "block px-4 py-2 text-sm text-gray-700"
+                                )}
+                              >
+                                Your Profile
+                              </a>
+                            )}
+                          </Menu.Item>
+                          <Menu.Item>
+                            {({ active }) => (
+                              <a
+                                href="#"
+                                className={classNames(
+                                  active ? "bg-gray-100" : "",
+                                  "block px-4 py-2 text-sm text-gray-700"
+                                )}
+                              >
+                                Settings
+                              </a>
+                            )}
+                          </Menu.Item>
+                          <Menu.Item>
+                            {({ active }) => (
+                              <a
+                                href="#"
+                                className={classNames(
+                                  active ? "bg-gray-100" : "",
+                                  "block px-4 py-2 text-sm text-gray-700"
+                                )}
+                                onClick={() => logout()}
+                              >
+                                Sign out
+                              </a>
+                            )}
+                          </Menu.Item>
+                        </Menu.Items>
+                      </Transition>
+                    </Menu>
+                  )}
+                </div>
+              </div>
             </div>
-          </Disclosure.Panel>
-        </>
-      )}
-    </Disclosure>
-    {/* <LoginModal data={open}/> */}
+
+            <Disclosure.Panel className="sm:hidden">
+              <div className="px-2 pt-2 pb-3 space-y-1">
+                {navigation.map((item) => (
+                  <Disclosure.Button
+                    key={item.name}
+                    as="a"
+                    href={item.href}
+                    className={classNames(
+                      item.current
+                        ? "bg-gray-900 text-white"
+                        : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                      "block px-3 py-2 rounded-md text-base font-medium"
+                    )}
+                    aria-current={item.current ? "page" : undefined}
+                  >
+                    {item.name}
+                  </Disclosure.Button>
+                ))}
+              </div>
+            </Disclosure.Panel>
+          </>
+        )}
+      </Disclosure>
+      {/* <LoginModal data={open}/> */}
     </>
-  )
+  );
 }
