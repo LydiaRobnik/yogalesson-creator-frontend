9eeadaf2a2aea0c6bea966145e08c8adc154e300
Lydia Robnik <84912419+LydiaRobnik@users.noreply.github.com>
2022-05-06 15:01:30 +0200

    footer not fixed yet

--- src/components/Planner/Planner.jsx
+++ src/components/Planner/Planner.jsx
@@ -2,7 +2,7 @@ import React, { useState, useContext, useEffect, useRef } from 'react';
 import { AuthContext } from '../../context/AuthContext';
 import { useNavigate, useOutletContext } from 'react-router-dom';
 import './planner.scss';
-import NewSequence from '../NewSequence.jsx/NewSequence';
+// import NewSequence from '../NewSequence.jsx/NewSequence';
 import SequencePlanned from '../SequencePlanned/SequencePlanned';
 import asanaService from '../../api/asanaService';
 
@@ -124,7 +124,7 @@ export default function Planner() {
               ))}
           </div>
 
-          {showNewSequence && <NewSequence handleFocus={handleFocus} />}
+          {/* {showNewSequence && <NewSequence handleFocus={handleFocus} />} */}
 
           <div className=" w-full flex flex-row justify-center">
             <button
