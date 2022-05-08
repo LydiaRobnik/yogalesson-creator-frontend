0efa1d16a97f308a3f3c2bd6f4cbf19f92cdfc16
Thomas <schmiddla@gmail.com>
2022-05-08 14:57:08 +0200

    profile proto

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -183,7 +183,7 @@ export default function Navbar() {
                           <Menu.Item>
                             {({ active }) => (
                               <NavLink
-                                to={'user/profile'}
+                                to={'/user/profile'}
                                 className={classNames(
                                   active ? 'bg-gray-100' : '',
                                   'block px-4 py-2 text-sm text-gray-700'
