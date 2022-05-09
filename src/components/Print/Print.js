c35c925c2891b117c46c78d33a776cf24ec198a6
VLacroixB <97441265+VLacroixB@users.noreply.github.com>
2022-05-09 16:15:49 +0200

    print button style changed

--- src/components/Print/Print.js
+++ src/components/Print/Print.js
@@ -1,6 +1,7 @@
 import React, { useRef } from "react";
 import { useReactToPrint } from "react-to-print";
 import SequencePlanned from "../SequencePlanned/SequencePlanned";
+import '../../style/variables.scss';
 
 const Print = ({sequence, handleFocus}) => {
   const componentRef = useRef();
@@ -12,7 +13,7 @@ const Print = ({sequence, handleFocus}) => {
 
   return (
     <>
-      <button onClick={handlePrint}>Print this out!</button>
+      <button className="btn-blue btn-blue:hover mx-2 flex flex-row items-center" onClick={handlePrint}>Print this out!</button>
       <SequencePlanned sequence={sequence} handleFocus={handleFocus} ref={componentRef} />
     </>
   );
