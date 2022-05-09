3b0c2df87f493837bca188d19e78537762947bf5
Thomas <schmiddla@gmail.com>
2022-05-09 10:22:57 +0200

    email input

--- src/components/Login/Loginform.jsx
+++ src/components/Login/Loginform.jsx
@@ -64,10 +64,10 @@ export default function Example({ ModalOpen, setModalOpen }) {
                 <input
                   id="username"
                   name="username"
-                  type="email"
+                  type="text"
                   required
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  placeholder="Email"
+                  placeholder="Email or Username"
                   onChange={(e) => setName(e.target.value)}
                   minLength={3}
                 />
--- src/components/Login/Signupform.jsx
+++ src/components/Login/Signupform.jsx
@@ -65,7 +65,7 @@ export default function Example({ SignupModal, setSignupModal }) {
                 <input
                   id="username"
                   name="username"
-                  type="text"
+                  type="email"
                   required
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="Email"
