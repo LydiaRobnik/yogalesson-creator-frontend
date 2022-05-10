eb0cabfe42021f41198f3a0c3426a816a974f788
Michael-Andreas <michaelandreas246@gmail.com>
2022-05-10 22:51:29 +0200

    signuoinfo styled

--- src/components/SignupInfo/SignupInfo.jsx
+++ src/components/SignupInfo/SignupInfo.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import './signupInfo.scss';
 
 export default function SignupInfo() {
   const navigate = useNavigate();
   // console.log('🔥', 'SignupInfo');
 
   return (
-    <div className="text-black flex justify-center">
+    <div className="cont1 text-black flex-col py-5">
       <p className="text-2xl">
         An E-Mail has been sent to you, please verify your E-Mail by clicking on
         the Verification-Link shown in the E-Mail.
       </p>
-      <button className="btn-neutra mx-2" onClick={() => navigate('/home')}>
+      <button
+        className="btn-bb bg-blue-light hover:bg-blue-middle p-2 mt-5"
+        onClick={() => navigate('/home')}
+      >
         go back to home
       </button>
     </div>
